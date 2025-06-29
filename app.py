14189de515e96056a8107fb96aa2031a0fc081b7
Ilyas Khasakhanov <i.khasahanov@neuromed.tech>
2025-06-29 18:42:19 +0300

    req

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from frontend.forms.scales.soba_recomendation import show_soba
 
 def show_sidebar():
     with st.sidebar:
-        if st.button("➗Алгоритм расчета анестезиологического риска", use_container_width=True):
+        if st.button("➗ Алгоритм расчета анестезиологического риска", use_container_width=True):
             st.session_state["stage"] = 0
 
 
